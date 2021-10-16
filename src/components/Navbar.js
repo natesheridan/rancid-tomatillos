827b12c5bd9cc73427aff43710d63d457abfff84
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-10-16 02:10:35 -0600

    Added render method to render nav buttons

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Navbar.css';
+
+class Navbar extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state={};
+    }
+//Ability to pass in current page/genre selected and highlight the button through props
+    render() {
+        return (
+            <nav className="navbar">
+                <button><a className="nav-btn">Test</a></button>
+                <button><a className="nav-btn">Test</a></button>
+                <button><a className="nav-btn">Test</a></button>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
