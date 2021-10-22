aebd9e1dd3780f5f4b12eef296ae05d26a4608ae
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-10-21 19:21:37 -0600

    Remove API calls from file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 // import api from './api';
 // import movieData from './movieData';
@@ -20,41 +19,7 @@ import Main from './components/Main.js'
 //   },...
 // }
 
-export const endpoints = {
-  movies: 'https://rancid-tomatillos.herokuapp.com/api/v2/movies',
-  /*
-    {"movies":
-      [{
-        id: 1,
-        title: "Movie Title",
-        poster_path: "someURL",
-        backdrop_path: "someURL",
-        release_date: "2019-12-04",
-        overview: "Some overview",
-        average_rating: 6 }, ...
-      ]}
-  */
-  movie: 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/:movie_id',
-  /*
-    An array of available videos corresponding to the movie whose id is in the URL;
-    this may be an empty array: [] or
-    [id: 1, movie_id: 1, key:"SUXWAEX2jlg", site: "YouTube", type:"Trailer"]
-  */
-  videos: 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/:movie_id/videos',
-  login: 'https://rancid-tomatillos.herokuapp.com/api/v2/login',
-  /*
-    POST: {email: <String>, password: <String>}
-  */
-  ratings: 'https://rancid-tomatillos.herokuapp.com/api/v2/users/:user_id/ratings',
-  /*
-    GET or
-    POST: { movie_id: <Integer>, rating: <Integer between 1 and 10> }
-  */
-  ratingsId: 'https://rancid-tomatillos.herokuapp.com/api/v2/users/:user_id/ratings/:rating_id'
-  /*
-    DELETE
-  */
-};
+
 
 function App() {
   return (
